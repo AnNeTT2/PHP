4832e6cfc41d30992edc79a918ddff169accf693
AnNeTT2 <anka7220@gmail>
2025-04-24 19:23:11 +0300

    Цепочки методов.

---
+++ Code Mu. ООП и MVC/Цепочки методов.php
@@ -0,0 +1,27 @@
+<!-- Не подсматривая в мой код самостоятельно реализуйте такой же класс Arr, методы которого будут вызываться в виде цепочки.-->
+<?php
+class Arr
+{
+    private $numbers = []; // задаем начальное значение свойства как []
+
+    public function add($number)
+    {
+        $this->numbers[] = $number;// добавляем переданное число в массив
+        return $this; // вернем ссылку сами на себя для цепочного вызова
+    }
+
+    public function push($numbers)
+    {
+        $this->numbers = array_merge($this->numbers, $numbers); // объединяем существующий массив $this->numbers с новым массивом $numbers
+        return $this; // вернем ссылку сами на себя для цепочного вызова
+    }
+
+    public function getSum()
+    {
+        return array_sum($this->numbers);
+    }
+}
+
+$arr = new Arr;
+echo $arr->add(1)->add(2)->push([3, 4])->getSum();
+?>
